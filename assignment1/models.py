c70bb1d56c5fe1fe69cebd846fe4f1148b1974c6
priyanka <patil.priyankaco@gmail.com>
2019-09-03 23:24:42 +0530

    Fix: Made Quote attribute "rating" optional

--- assignment1/models.py
+++ assignment1/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from rest_framework import serializers
 
 class Quote(models.Model):
     quote = models.TextField(max_length=1024)
     author = models.CharField(max_length=64)
-    rating = models.DecimalField(max_digits=10,decimal_places=2)
+    rating = models.DecimalField(max_digits=10,decimal_places=2,blank=True,null=True)
+
+# class QuoteSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Quote
+#         fields = ["quote", "author", "rating"]
